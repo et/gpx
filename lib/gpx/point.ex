c51a93d409c366e07e04472523f1bf323f25a3b6
Eric Thomas <eric.l.m.thomas@gmail.com>
2016-10-31 09:38:48 -0600

    add gpx point

---
+++ lib/gpx/point.ex
@@ -0,0 +1,11 @@
+defmodule Gpx.Point do
+  @moduledoc """
+  A `Gpx.Point` is a struct representation of a GPX trackpoint.
+  """
+
+  @type t :: %__MODULE__{
+    lat: float,
+    lon: float,
+    ele: float
+  }
+end
